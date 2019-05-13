34c3dc7224bdb77660cc7c16ee14c894675397fb
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-05-14 01:16:19 +0900

    色指定できないエラー

---
+++ T22.java
@@ -0,0 +1,22 @@
+import tg.*; // import宣言 tgパッケージを参照
+import javafx.scene.paint.*; // import宣言 javafx.scene.paintパッケージを参照
+public class T22 {
+  public static void main(String[] args){
+    double x = 300.0, y = 200.0, d = 100.0; // double 型の変数を用意
+    TurtleFrame f = new TurtleFrame(700, 500); //引数のあるコンストラクタ呼出し
+    Turtle m = new Turtle(x, y, 180.0);
+    Turtle m1 = new Turtle(x+d, y+d, 0.0);
+    Color c = new Color(0.8, 0.0, 0.0, 1.0); //赤色
+    m1.setColor(Green); //m1 の色をcに指定
+    f.add(m);
+    f.add(m1);
+    m.fd(d);
+    m1.fd(d);
+    m.lt(90.0);
+    m1.lt(90.0);
+    d = d / 2; //d の値を d/2 に変更
+    m.fd(d);
+    m1.fd(d);
+    m1.moveTo(m);
+  }
+}

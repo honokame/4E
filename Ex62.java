22e46b350817b0492aebaec4f66f7d03e393662a
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-05-27 15:05:20 +0900

    new file

---
+++ Ex62.java
@@ -0,0 +1,10 @@
+public class Ex62{
+  public static void main(String[] args){
+    Student st = new Student();
+    st.setStudent("e0000");
+    st.setStudent(91.5);
+    st.display();
+    st.setStudent("e0001", 65.1);
+    st.display();
+  }
+}

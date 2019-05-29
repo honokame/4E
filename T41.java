4c1be4db72a8202057c7e5be2b6f1a91e4d93d58
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-05-29 23:16:58 +0900

    cpで編集

--- T41.java
+++ T41.java
@@ -3,18 +3,16 @@ public class T41{
   public static void main(String[] args){
     int i;
     double x,e;
-<<<<<<< HEAD
-
-=======
     
->>>>>>> origin/master
     for(i = 0;i < 10000;i++){
       x = random();
       e = pow((sin(x * PI / 180) + log(x)),2);
+      
       if(i == 10000){
         System.out.println("近似値解は得られなかった");
         System.exit(0);
       }
+      
       if(e < 0.00001){
         i++;
         System.out.println("近似値解は"+x);

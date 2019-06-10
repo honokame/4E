848fa452b05b08a045bdf531eda02470e02204c9
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-06-10 23:55:37 +0900

    コメント

--- Ex62.java
+++ Ex62.java
@@ -1,10 +1,10 @@
 public class Ex62{
   public static void main(String[] args){
     Student st = new Student();
-    st.setStudent("e0000");
-    st.setStudent(91.5);
-    st.display();
-    st.setStudent("e0001", 65.1);
-    st.display();
+    st.setStudent("e0000"); // setStudentにe0000を渡す
+    st.setStudent(91.5); // setStudentに91.5を渡す
+    st.display(); // 表示する
+    st.setStudent("e0001",65.1); // setStudentにe0001,65.1を渡す
+    st.display(); // 表示する
   }
 }

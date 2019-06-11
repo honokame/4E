ad48f6c834b64b8f2390966dd9ebe6ea43f545f4
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-06-11 11:22:50 +0900

    不要な関数も利用するようにした

--- Student.java
+++ Student.java
@@ -2,43 +2,56 @@ public class Student{
   private String studentID;
   private double score;
 
+  // studentIDを代入
   public void setStudentID(String studentID){
     this.studentID = studentID;
   }
 
+  // studentIDを返す
   public String getStudent(){
     return studentID;
   }
   
+  // scoreを代入(0以上)
   public void setScore(double score){
     if(score > 0){
       this.score = score;
     }
-    else{
-      System.out.println("値が無効です");
-    }
   }
 
+  // scoreを返す
   public double getScore(){
     return score;
   }
   
+  // settudentIDにstudentIDを渡す
   public void setStudent(String studentID){
-    this.studentID = studentID;
+    setStudentID(studentID);
   }
 
+  //  setScoreにscoreを渡す
   public void setStudent(double score){
-    this.score = score;
+    setScore(score);
   }
-
+  
+  // setStudet、setScoreにstudentID、scoreを渡す
   public void setStudent(String studentID,double score){
-    this.studentID = studentID;
-    this.score = score;
+    setStudentID(studentID);
+    setScore(score);
   }
 
+  // 表示
   public void display(){
+    // 学籍番号表示
     System.out.println("学籍番号は"+studentID+"です。");
-    System.out.println("得点は"+score+"です。");
+    
+    // 得点表示
+    if(score > 0){
+      System.out.println("得点は"+score+"です。");
+    }
+    else{
+      System.out.println("値が無効です（score）");
+    }
   }
 }
 

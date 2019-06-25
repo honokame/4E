4dcc0c3b961f62a2ff2ad33fc828697d6eed5b7e
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-06-26 08:39:31 +0900

    近似の範囲を変更

--- Newton.java
+++ Newton.java
@@ -1,15 +1,17 @@
 import static java.lang.Math.*;
 public class Newton{ 
   public static void main(String[] args){ 
-   double[] x = {0.2};
-   int i = 0;
+    double[] x = new double[1000];
+    int i = 0;
+    x[0] = 0.2;
 
-    for(i = 0;i < 1000;i++){
-      x[i + 1] = x[i] - (0.5 - x[i] + (0.2 * sin(x[i]))/-1 + (0.2 * cos(x[i])));
-      if(0.5 - x[i + 1] + (0.2 * sin(x[i + 1])) == 0){
+    for(i = 0;i < 999;i++){
+      x[i + 1] = x[i] - (0.5 - x[i] + ((0.2 * sin(x[i])))/(-1 + (0.2 * cos(x[i]))));
+      if(0.5 - x[i + 1] + (0.2 * sin(x[i + 1])) >= 0.00001){
         System.out.println("x = "+x[i + 1]);
+        break;
       }
     }
-    // return i;
-  }        
+  }
 }
+

095b95e8d0387a3b6fbb5e8b66a0bb3fdf2744ed
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-06-25 22:00:52 +0900

    Student.javaの実行

---
+++ Ex72.java
@@ -0,0 +1,8 @@
+public class Ex72{
+  public static void main(String[] args){
+    Student st0 = new Student(); // st0に初期値を代入する
+    st0.display(); // 表示する
+    Student st1 = new Student("e1234",97.5); // st1にe1234,97.5を渡す
+    st1.display(); // 表示する
+  }
+}

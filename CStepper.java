e61622f1efcab416bc8ef87ec7130bb24681eaeb
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-06-27 21:41:17 +0900

    コメント

--- CStepper.java
+++ CStepper.java
@@ -3,21 +3,29 @@ public class CStepper extends Turtle{
   private int n;
   private double size;
   private int j = 0;
-  public CStepper(int n, double size) {
+  
+  // 一辺がsizeのn角形を作成
+  public CStepper(int n,double size){ // コンストラクタ（クラス名のメソッド）
     this.n = n; 
     this.size = size; 
   }
-  
-  public CStepper(int n, double size, double x,double y, double angle) {
-    super(x, y, angle);
+
+  // 一辺がsizeのn角形を(x,y,angle)に作成 
+  public CStepper(int n,double size,double x,double y,double angle){ // コンストラクタ（クラス名のメソッド）
+    super(x,y,angle); // Turtleのコンストラクタ呼び出し
     this.n = n;
     this.size = size;
   }
-  
-  public void step() {
+
+  public void step(){
+
+    // 描き終えているか
     if(j >= n){
-      return; fd(size);
+      return; 
     }
+
+    // 一回分描く
+    fd(size);
     rt(360.0/n);
     j++;
   }

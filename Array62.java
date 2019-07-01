a2c3b92e3cbd17e83634c464514c6e0d63b80a77
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-01 16:02:46 +0900

    コメント

--- Array62.java
+++ Array62.java
@@ -1,4 +1,4 @@
-public class Array62 {
+public class Array62{
   public static void main(String[] args){
     int[] data= {100,50,200,100,0,200,102,40,30,100};
     show(data);
@@ -6,49 +6,56 @@ public class Array62 {
     System.out.println("平均：" + average(data));
     System.out.println("最大値：" + max(data));
     System.out.println("最大値を与える一番小さいインデックス：" + maxIndex(data));
-    System.out.println("100 以上の個数：" +
-        numGreater(data,100));
+    System.out.println("100 以上の個数：" + numGreater(data,100));
   }
 
-  static void show(int... a) {
-    for(int i = 0; i < a.length; i++){
+  static void show(int... a){
+    for(int i = 0;i < a.length;i++){
       System.out.print(a[i] + ", ");
     }
+    
     System.out.println("");
   }
 
   static int sum(int[] a){
     int sum = 0;
-    for(int i = 0; i < a.length; i++){
+    for(int i = 0;i < a.length;i++){
       sum += a[i];
     }
+
     return sum;
   }
 
   static int average(int[] a){
-    return sum(a)/a.length
+    return sum(a) / a.length
   }
 
   static int max(int[] a){
     int max = a[0];
-    for(int i = 1; i < a.length; i++){
-      if (a[i] > max) max = a[i];
+    for(int i = 1;i < a.length;i++){
+      if (a[i] > max){
+        max = a[i];
+      }
     }
     return max;
   }
 
   static int maxIndex(int[] a){
     int maxind = 0;
-    for(int i = 1; i < a.length; i++){
-      if (a[i] > a[maxind]) maxind = i;
+    for(int i = 1;i < a.length;i++){
+      if (a[i] > a[maxind]){
+        maxind = i;
+      }
     }
     return maxind;
   }
 
-  static int numGreater(int[] a, int n){
+  static int numGreater(int[] a,int n){
     int c = 0;
-    for(int i = 0; i < a.length; i++){
-      if (a[i] >= n) c++;
+    for(int i = 0;i < a.length;i++){
+      if (a[i] >= n){
+        c++;
+      }
     }
     return c;
   }

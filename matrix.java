36960223f2abe70f85032beb72c5d769a6bce78b
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-16 12:38:05 +0900

    scannerを利用して、変換した

--- matrix.java
+++ matrix.java
@@ -1,20 +1,23 @@
+import java.util.Scanner;
 public class matrix{
-  public static void main(String[] args){ // 行列の引数
-    // 入力された行列を表示する
-    System.out.println("入力された行列は");
-    System.out.print(args[0] + "\t" + args[1] + "\n" + args[2] + "\t" + args[3]  + "\n");    
-    
+  public static void main(String[] args){ 
     double [] matrix = new double[3];
-    for(int i = 0;i < 3;i++){
-      matrix[i] = args[i];
-    }
-
+    
+    // 行列を入力し、double型に変換    
+    Scanner scan = new Scanner(System.in);
+    System.out.println("行列を入力してください");
+    // System.out.print(args[0] + "\t" + args[1] + "\n" + args[2] + "\t" + args[3]  + "\n");    
+    // Scanner = scan = new Scanner(System.in);
+    // double [] matrix = new double[3];
+    for(int i = 0;i < 2;i++){
+      matrix[i] = Double.parseDouble(scan.next());
+    }/*
     double frac = 1 / ((matrix[0] * matrix[3]) - (matrix[1] * matrix[2]));
     double[] imatrix = new double[3];
     imatrix[0] = frac * matrix[3];
     imatrix[1] = frac * matrix[1] * -1;
     imatrix[2] = frac * matrix[2] * -1;
     imatrix[3] = frac * matrix[0];
-    System.out.print(imatrix[0] + "\t" + imatrix[1] + "\n" + imatrix[2] + "\t" + imatrix[3]  + "\n");    
+    System.out.print(imatrix[0] + "\t" + imatrix[1] + "\n" + imatrix[2] + "\t" + imatrix[3]  + "\n");    */
   }
 }

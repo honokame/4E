04f091a9c7dca1c20e464a70f6632d31dd0825fe
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-16 11:31:03 +0900

    comment and indent

--- Args61.java
+++ Args61.java
@@ -1,8 +1,11 @@
 public class Args61{
   public static void main(String[] args){
-    System.out.println(args.length + "個の引数：");
+    System.out.println(args.length + "個の引数："); // argsの引数の数を表示
+    
+    // i番目の引数を表示する
     for(int i = 0;i < args.length;i++){
       System.out.println(i + "番目の引数" + args[i]);
     }
   }
 }
+

443f2cf9550719ced21b7187075eb0d0b2e75840
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-16 12:52:47 +0900

    配列の要素数が間違ってエラーが出ていたのを修正

--- matrix.java
+++ matrix.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class matrix{
   public static void main(String[] args){ 
-    double [] matrix = new double[3];
+    double[] matrix = new double[4];
     
     // 行列を入力し、double型に変換    
     Scanner scan = new Scanner(System.in);
@@ -9,15 +9,19 @@ public class matrix{
     // System.out.print(args[0] + "\t" + args[1] + "\n" + args[2] + "\t" + args[3]  + "\n");    
     // Scanner = scan = new Scanner(System.in);
     // double [] matrix = new double[3];
-    for(int i = 0;i < 2;i++){
-      matrix[i] = Double.parseDouble(scan.next());
-    }/*
+    //for(int i = 0;i < 4;i++){
+       matrix[1] = Double.parseDouble(scan.next());
+       matrix[1] = Double.parseDouble(scan.next());
+       matrix[2] = Double.parseDouble(scan.next());
+       matrix[3] = Double.parseDouble(scan.next());
+       
+    //}
     double frac = 1 / ((matrix[0] * matrix[3]) - (matrix[1] * matrix[2]));
-    double[] imatrix = new double[3];
+    double[] imatrix = new double[4];
     imatrix[0] = frac * matrix[3];
     imatrix[1] = frac * matrix[1] * -1;
     imatrix[2] = frac * matrix[2] * -1;
     imatrix[3] = frac * matrix[0];
-    System.out.print(imatrix[0] + "\t" + imatrix[1] + "\n" + imatrix[2] + "\t" + imatrix[3]  + "\n");    */
+    System.out.print(imatrix[0] + "\t" + imatrix[1] + "\n" + imatrix[2] + "\t" + imatrix[3]  + "\n");    
   }
 }

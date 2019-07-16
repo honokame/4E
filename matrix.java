09905197190fc569fe90cffce498aa2cc3832288
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-16 12:19:51 +0900

    argsをdouble型に変換した

--- matrix.java
+++ matrix.java
@@ -1,15 +1,20 @@
 public class matrix{
-  public static void main(double[] args){ // 行列の引数
+  public static void main(String[] args){ // 行列の引数
     // 入力された行列を表示する
     System.out.println("入力された行列は");
     System.out.print(args[0] + "\t" + args[1] + "\n" + args[2] + "\t" + args[3]  + "\n");    
     
-    double frac = 1 / ((args[0] * args[3]) - (args[1] * args[2]));
-    double[] imatrix = new double[3];
-    imatrix[0] = frac * args[3];
-    imatrix[1] = frac * args[1] * -1;
-    imatrix[2] = frac * args[2] * -1;
-    imatrix[3] = frac * args[0];
+    double [] matrix = new double[3];
+    for(int i = 0;i < 3;i++){
+      matrix[i] = args[i];
+    }
 
+    double frac = 1 / ((matrix[0] * matrix[3]) - (matrix[1] * matrix[2]));
+    double[] imatrix = new double[3];
+    imatrix[0] = frac * matrix[3];
+    imatrix[1] = frac * matrix[1] * -1;
+    imatrix[2] = frac * matrix[2] * -1;
+    imatrix[3] = frac * matrix[0];
+    System.out.print(imatrix[0] + "\t" + imatrix[1] + "\n" + imatrix[2] + "\t" + imatrix[3]  + "\n");    
   }
 }

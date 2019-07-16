4acc5cc874df63c1fa80fc5a0fca6e9afb32cd40
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-16 11:24:29 +0900

    comment and indent

--- T62.java
+++ T62.java
@@ -3,12 +3,16 @@ public class T62{
   public static void main(String[] args){
     TurtleFrame f = new TurtleFrame(600,300);
     HTurtle[][] tm = new HTurtle[3][4];
+    
+    // それぞれのカメを定義する
     for(int i = 0;i < 3;i++){
       for(int j = 0;j < 4;j++){
         tm[i][j] = new HTurtle(j * 50 + 50,i * 50+ 50,0);
-        f.add(tm[i][j]);
+        f.add(tm[i][j]); // カメ出現
       }
     }
+
+    // 一匹ずつ描く
     for(int i = 0;i < 3;i++){
       for(int j = 0;j < 4;j++){
         tm[i][j].polygon(i + 3,(j + 1) * 10);
--- T63.java
+++ T63.java
@@ -2,14 +2,20 @@ import tg.*;
 public class T63{
   public static void main(String[] args){
     TurtleFrame f = new TurtleFrame(600,300);
-    HTurtle[][] tm = new HTurtle[3][];
+    HTurtle[][] tm = new HTurtle[3][]; // 配列の配列を生成
+    
+    // i行目の配列の大きさを定義する
     for(int i = 0;i < tm.length;i++){
       tm[i] = new HTurtle[i + 2];
+
+      // それぞれのカメを定義する
       for(int j = 0;j < tm[i].length;j++){
-        tm[i][j] = new HTurtle(j * 50 + 50,i * 50+ 50,0);
+        tm[i][j] = new HTurtle(j * 50 + 50,i * 50 + 50,0);
         f.add(tm[i][j]);
       }
     }
+
+    // 一匹ずつ描く
     for(int i = 0;i < tm.length;i++){
       for(int j = 0;j < tm[i].length;j++){
         tm[i][j].polygon(i + 3,(j + 1) * 10);

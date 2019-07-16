9ad224cb756174a2ca66f9443fc91139a50e5718
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-16 12:12:30 +0900

    引数をdoubleにした、逆行列の計算ができる

--- matrix.java
+++ matrix.java
@@ -1,6 +1,15 @@
 public class matrix{
-  public static void main(String[] args){ // 行列の引数
+  public static void main(double[] args){ // 行列の引数
+    // 入力された行列を表示する
     System.out.println("入力された行列は");
     System.out.print(args[0] + "\t" + args[1] + "\n" + args[2] + "\t" + args[3]  + "\n");    
+    
+    double frac = 1 / ((args[0] * args[3]) - (args[1] * args[2]));
+    double[] imatrix = new double[3];
+    imatrix[0] = frac * args[3];
+    imatrix[1] = frac * args[1] * -1;
+    imatrix[2] = frac * args[2] * -1;
+    imatrix[3] = frac * args[0];
+
   }
 }

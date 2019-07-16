bbb3ab6b815be8b53527c04c1c5fccd1b16a7ded
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-16 11:43:50 +0900

    入力された行列を表示できる

---
+++ matrix.java
@@ -0,0 +1,8 @@
+public class matrix{
+  public static void main(String[] args){ // 行列の引数
+    System.out.println("入力された行列は");
+    for(int i = 0;i < args.length;i++){
+      System.out.println(args[i]);
+    }
+  }
+}

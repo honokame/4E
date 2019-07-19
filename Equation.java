084b2a34d071943939a561428179fddfdbade6b8
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-19 09:47:01 +0900

    add comment

--- Equation.java
+++ Equation.java
@@ -4,10 +4,12 @@ public class Equation{
     int a,b,c;
     Scanner scan = new Scanner(System.in);
     System.out.println("二次方程式 ax^2 + bx + c に対するa,b,cを入力してください");
+
+    // string型からint型に変換
     a = Integer.parseInt(scan.next()); 
     b = Integer.parseInt(scan.next()); 
     c = Integer.parseInt(scan.next());
-    System.out.println("判別式は" + Discriminant.discriminant(a,b,c));
-    SolveQE.solveQE(a,b,c);
+    System.out.println("判別式は" + Discriminant.discriminant(a,b,c)); // 判別式の値を出力
+    SolveQE.solveQE(a,b,c); // 二次方程式の解を出力
   }
 }
--- SolveQE.java
+++ SolveQE.java
@@ -1,16 +1,22 @@
+import java.lang.Math
 public class SolveQE{
   public static void solveQE(int a,int b,int c){
     double x1,x2,d;
-    d = Discriminant.discriminant(a,b,c);
+    d = Discriminant.discriminant(a,b,c); // 判別式の値をdに代入
+    x1 = (-b + sqrt(d)) / (2 * a);
+    x2 = (-b - sqrt(d)) / (2 * a);
+
+    // 解が2つ
     if(d > 0){
-       x1 = (-b + Math.sqrt(d)) / (2 * a);
-       x2 = (-b - Math.sqrt(d)) / (2 * a);
        System.out.println("解：x =" + x1 + "," + x2);
     }
+
+    // 解が1つ
     else if(d == 0){
-      x1 = (-b + Math.sqrt(d)) / (2 * a);
       System.out.println("解：x =" + x1);
     }
+
+    // 実数解が存在しない
     else{
       System.out.println("実数解は存在しません");
     }

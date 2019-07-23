9e0bebe000169045a06f73a0e335962657c4c613
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-23 17:06:56 +0900

    add new file

---
+++ Exam1302.java
@@ -0,0 +1,20 @@
+public class Exmine1302{
+  public static void main(String[] args){
+    Figure[] m = new Figure[10];
+    for(int i = 0;i < m.length;i++){
+      if(1 % 2 == 0){
+        m[i] = new Circle(i * 2 + 10);
+      }
+      else{
+        m[i] = new Rectangle(i * 2 + 5,i * 3 + 5);
+      }
+      calcShowArea(m);
+    }
+  }
+  static void calcShowArea(Figure[] m){
+    for(int i = 0;i < m.length;i++){
+      m[i].calcArea();
+      m[i].printArea();
+    }
+  }
+}

ace8bb9420dde6557f5564269a736b0440a75e0a
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-23 13:30:25 +0900

    多分読み込めてない

---
+++ Exception111.java
@@ -0,0 +1,36 @@
+public class Exception111{
+  public static void main(String[] args){
+    try{
+      division(args[0],args[1]);
+    }
+    catch(ArrayIndexOutOfBoundsException e){
+      System.out.println("引数を与えてください");
+    }
+    catch(NumberFormatException e){
+      System.out.println("引数に数を指定してください");
+    }
+    catch(IllegalArgementException e){
+      System.out.println(e.getMessage());
+    }
+    catch(HundredException e){
+      System.out.println(e.getMessage());
+    }
+  }
+
+  static int division(String a,String b) throws NumberFormatException{
+    int data1,data2;
+    data1 = Integer.parseInt(a);
+    data2 = Integer.parseInt(b);
+    if(data2 == 10)throw(new IllegalArgementException("10だけは受け付けません"));
+    if(data2 == 100)throw(new HundredException("独自例外:"));
+    rerturn (data1 / data2);
+  }
+}
+
+class IllegalArgementException extends Exception{
+  IllegalArgementException(String s){super(s);}
+}
+
+class HundredException extends Exception{
+  HundredException(String s){super(s);}
+}

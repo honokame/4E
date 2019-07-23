a63554f297a75f231ff2fc5d6114bd0019763a8f
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-23 15:09:59 +0900

    throws追加

--- Exception112.java
+++ Exception112.java
@@ -1,38 +1,41 @@
-class IllegalArgementException extends Exception{
-  IllegalArgementException(String s){super(s);}
-}
-
-class HundredException extends Exception{
-  HundredException(String s){super(s);}
-}
-public class Exception111{
+public class Exception112{
   public static void main(String[] args){
+    int data1 = 0,data2 = 0;
+    
+    // 正常時実行 
     try{
-      System.out.println(division(args[0],args[1]));
+      // String型からint型に変換
+      data1 = Integer.parseInt(args[0]); 
+      data2 = Integer.parseInt(args[1]);
+
+      // 除数が0 割れない時
+      if(data2 == 0){
+        System.out.println("エラー：除数が0");
+      }
+
+      // 割れる時
+      else{
+        System.out.println(division(args[0],args[1]));
+      }
     }
+
+    // 引数が足りない時
     catch(ArrayIndexOutOfBoundsException e){
       System.out.println("引数を与えてください");
     }
-    catch(NumberFormatException e){
-      System.out.println("引数に数を指定してくださいまたは");
-      System.out.println(e.getMessage());
 
-    }
-    catch(IllegalArgementException e){
-      System.out.println(e.getMessage());
-    }
-    catch(HundredException e){
+    // 引数が数値以外の時
+    catch(NumberFormatException e){
+      System.out.println("引数に数を指定してください");
       System.out.println(e.getMessage());
     }
   }
 
-  static int division(String a,String b) throws IllegalArgementException,HundredException{
+  // 商を求める
+  static int division(String a,String b) throws NumberFormatException{
     int data1,data2;
     data1 = Integer.parseInt(a);
     data2 = Integer.parseInt(b);
-    if(data2 == 0)throw(new NumberFormatException("除数が0"));
-    if(data2 == 10)throw(new IllegalArgementException("10だけは受け付けません"));
-    if(data2 == 100)throw(new HundredException("独自例外:"+data2));
     return data1 / data2;
   }
 }

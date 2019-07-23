171ed128be4e97daf5775169a7284451979a3622
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-23 14:50:35 +0900

    0の処理ができない

--- Exception111.java
+++ Exception111.java
@@ -1,38 +1,40 @@
-class IllegalArgementException extends Exception{
-  IllegalArgementException(String s){super(s);}
-}
-
-class HundredException extends Exception{
-  HundredException(String s){super(s);}
-}
 public class Exception111{
   public static void main(String[] args){
-    try{
-      System.out.println(division(args[0],args[1]));
-    }
-    catch(ArrayIndexOutOfBoundsException e){
-      System.out.println("引数を与えてください");
-    }
-    catch(NumberFormatException e){
-      System.out.println("引数に数を指定してくださいまたは");
-      System.out.println(e.getMessage());
+    //try{
+    System.out.println(division(args[0],args[1]));
+    /* }
+       catch(ArrayIndexOutOfBoundsException e){
+       System.out.println("引数を与えてください");
+       }
+       catch(NumberFormatException e){
+       System.out.println("引数に数を指定してください");
+       System.out.println(e.getMessage());
 
-    }
-    catch(IllegalArgementException e){
-      System.out.println(e.getMessage());
-    }
-    catch(HundredException e){
-      System.out.println(e.getMessage());
-    }
+       }*/
   }
 
-  static int division(String a,String b) throws IllegalArgementException,HundredException{
-    int data1,data2;
-    data1 = Integer.parseInt(a);
-    data2 = Integer.parseInt(b);
-    if(data2 == 0)throw(new NumberFormatException("除数が0"));
-    if(data2 == 10)throw(new IllegalArgementException("10だけは受け付けません"));
-    if(data2 == 100)throw(new HundredException("独自例外:"+data2));
-    return data1 / data2;
+  static int division(String a,String b){
+    int data1 = 0,data2 = 0;
+    try{
+      data1 = Integer.parseInt(a);
+      data2 = Integer.parseInt(b);
+    }
+    catch(NumberFormatException e){
+      System.out.println("引数に数字を指定してください");
+      return 0;
+    } if(data2 == 0){
+      System.out.println("エラー：除数が0");
+      return 0;
+    }
+
+    finally{
+      return data1 / data2;
+    }
+    /*if(data2 == 0){
+      System.out.println("エラー：除数が0");
+      return 0;
+    }*/
+    return 0;
+
   }
 }

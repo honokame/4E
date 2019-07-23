afadc7acf2779e7c1fdfcdb00ecabea68ef292dc
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-23 16:42:10 +0900

    にゃーん

--- P91.java
+++ P91.java
@@ -2,10 +2,10 @@ import tg.*;
 public class P91{ 
   public static void main(String[] args){ 
     TurtleFrame f = new TurtleFrame(); 
-    Turtle m = new HTurtle();    //HTurtle を生成して Turtle 型の m に代入 
+    Turtle m = new HTurtle(); // HTurtleを生成しTurtle型のmに代入 
     f.add(m);             
     m.fd(100);       
-    if(m  instanceof HTurtle){ 
+    if(m instanceof HTurtle){ 
       ((HTurtle) m).house(50); 
     } 
   } 

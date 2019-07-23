5fad5c2ccbd041f7dc2bbfcc32855f9fcc52d492
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-23 15:04:47 +0900

    add comment

--- Exception111.java
+++ Exception111.java
@@ -1,25 +1,37 @@
 public class Exception111{
   public static void main(String[] args){
     int data1 = 0,data2 = 0;
+    
+    // 正常時実行 
     try{
-      data1 = Integer.parseInt(args[0]);
+      // String型からint型に変換
+      data1 = Integer.parseInt(args[0]); 
       data2 = Integer.parseInt(args[1]);
+
+      // 除数が0 割れない時
       if(data2 == 0){
         System.out.println("エラー：除数が0");
       }
+
+      // 割れる時
       else{
         System.out.println(division(data1,data2));
       }
     }
+
+    // 引数が足りない時
     catch(ArrayIndexOutOfBoundsException e){
       System.out.println("引数を与えてください");
     }
+
+    // 引数が数値以外の時
     catch(NumberFormatException e){
       System.out.println("引数に数を指定してください");
       System.out.println(e.getMessage());
     }
   }
-
+　
+  // 商を求める
   static int division(int a,int b){
     return a / b;
   }

9817757f2b0586aadf5bc4ab9a1612e70fef5e1d
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-07-23 14:58:00 +0900

    divisionの処理をmainに戻した

--- Exception111.java
+++ Exception111.java
@@ -1,40 +1,29 @@
 public class Exception111{
   public static void main(String[] args){
-    //try{
-    System.out.println(division(args[0],args[1]));
-    /* }
-       catch(ArrayIndexOutOfBoundsException e){
-       System.out.println("引数を与えてください");
-       }
-       catch(NumberFormatException e){
-       System.out.println("引数に数を指定してください");
-       System.out.println(e.getMessage());
-
-       }*/
-  }
-
-  static int division(String a,String b){
     int data1 = 0,data2 = 0;
     try{
-      data1 = Integer.parseInt(a);
-      data2 = Integer.parseInt(b);
+      data1 = Integer.parseInt(args[0]);
+      data2 = Integer.parseInt(args[1]);
+      if(data2 == 0){
+        System.out.println("エラー：除数が0");
+      }
+      else{
+        System.out.println(division(data1,data2));
+
+      }
+    }
+    catch(ArrayIndexOutOfBoundsException e){
+      System.out.println("引数を与えてください");
     }
     catch(NumberFormatException e){
-      System.out.println("引数に数字を指定してください");
-      return 0;
-    } if(data2 == 0){
-      System.out.println("エラー：除数が0");
-      return 0;
-    }
+      System.out.println("引数に数を指定してください");
+      System.out.println(e.getMessage());
 
-    finally{
-      return data1 / data2;
     }
-    /*if(data2 == 0){
-      System.out.println("エラー：除数が0");
-      return 0;
-    }*/
-    return 0;
+  }
+
+  static int division(int a,int b){
+    return a / b;
 
   }
 }

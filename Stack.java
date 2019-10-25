f3f9b6c5d35d650f056ff45c584bc1cea5f44180
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-10-25 14:04:02 +0900

    ??

--- Stack.java
+++ Stack.java
@@ -10,11 +10,34 @@ public class Stack<T>{
   }
 
   // サイズ指定あり
-  public Stack(int size){
-    this.StackSize = size; // 指定されたサイズのstackを生成
+  public Stack(int StackSize){
+    this.StackSize = Stacksize; // 指定されたサイズのstackを生成
     this.StackPointer = 0; // stackポインタの初期化
   }
 
+
   // push
-  public 
+  public void push(T data){
+    if(StackPointer >= stack.length){
+      throw new RuntimeException("stack full");
+    }
+    
+    this.stack.add(data);
+    this.StackPointer++;
+  }
+
+  // pop
+  public T pop(){
+    T send;
+
+    if(StackPointer <= 0){
+      throw new RuntimeException("stack empty");
+    }
+    else{
+    this.StackPointer--;
+    
+    return this.stack()
+
+      
+
 

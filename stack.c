ee2b676e1912e26e389f4f2bf88fa4e3e3672055
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-11-13 21:26:17 +0900

    提出用完成

--- stack.c
+++ stack.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// stackの構造体
 typedef struct stack1{
   int data;
   struct stack1 *next;
 } stack;
 
+// グローバル変数
+int x;
+
+// メモリを確保　先頭アドレスを返す
 stack *memory(void){
   return ((stack *) malloc(sizeof(stack)));
 } 
 
+// push 先頭ポインタを返す
 stack *push(int x,stack *top){
   stack *p;
   p = memory();
@@ -18,9 +24,10 @@ stack *push(int x,stack *top){
   return p;
 }
 
+// pop 先頭ポインタを返す
 stack *pop(stack *top){
-  int x;
-
+  
+  // アンダーフロー
   if(top == NULL){
     printf("pop NG\n");
   }
@@ -31,36 +38,43 @@ stack *pop(stack *top){
   return top;
 }
 
+// メイン関数
 int main(void){
   stack *top = NULL;
   char input[20],command;
   int no;
 
   printf("i 数字(数字を挿入),d(削除),m 数字(数字分削除),p(出力),q(終了)を入力してください。\n");
-
+　
+  // stackの無限ループ
   while(1){
-    fgets(input,10,stdin);
-    sscanf(input,"%c %d",&command,&no);
+    fgets(input,10,stdin); // 入力読み取り
+    sscanf(input,"%c %d",&command,&no); // 文字と数字に変換
 
     switch(command){
-      case 'i':
+      case 'i': // 挿入
         top = push(no,top);
         break;
-      case 'd':
+      
+      case 'd': // 削除
         top = pop(top);
         break;
-      case 'm':
+      
+      case 'm': // 複数削除
         for(int i = 0;i < no;i++){
           top = pop(top);
         }
         break;
-      case 'p':
+      
+      case 'p': // 表示
         while(top != NULL){
           top = pop(top);
-          printf("%d",top);
+          printf("%d ",x);
         }
+        printf("\n");
         break;
-      case 'q':
+      
+      case 'q': // 終了
         return 0;
     }
   }

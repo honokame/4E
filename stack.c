d6868663a20dc1b2f0ac1031a527a653a87d4d4b
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-11-13 16:56:22 +0900

    入力からコマンドと数字を区別できた

--- stack.c
+++ stack.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+//#include<string.h>
 
 typedef struct stack1{
   int data;
   struct stack1 *next;
-  //struct stack1 *top = NULL;
 } stack;
-//stack *top = NULL;
-//int stackdata[5];
-//int stackpointer = 0;
 
 stack *memory(void){
   return ((stack *) malloc(sizeof(stack)));
@@ -34,26 +31,22 @@ stack *pop(stack *top){
     top = top->next;
     printf("pop OK %d\n",x);
   }
-  
-  /*if(top != NULL){
-    x  = top->data;
-    top = top->next;
-    printf("%d\n",x);
-  }
-  else
-    printf("pop NG\n");
-  */
   return top;
 }
 
 int main(void){
-  //int stackdata[5];
-  //int stackpointer = 0;
   stack *top = NULL;
 
-  top = push(2,top);
+  /*top = push(2,top);
   top = push(5,top);
   pop(top);
-
+*/
+  char input[20],command[5];
+  int no;
+  
+  fgets(input,10,stdin);
+  sscanf(input,"%s %d",command,&no);
+  printf("%s\n",command);
+  printf("%d\n",no);
   return 0;
 }

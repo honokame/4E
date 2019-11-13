caaa4211c3a7663604dd3a178df4a72c91bcfa52
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-11-13 15:14:44 +0900

    topの宣言、アドレスの代入、poshとpopのテストOK

--- stack.c
+++ stack.c
@@ -4,10 +4,11 @@
 typedef struct stack1{
   int data;
   struct stack1 *next;
-//  struct stack1 *top = NULL;
+  //struct stack1 *top = NULL;
 } stack;
-
-struct stack *top = NULL;
+//stack *top = NULL;
+//int stackdata[5];
+//int stackpointer = 0;
 
 stack *memory(void){
   return ((stack *) malloc(sizeof(stack)));
@@ -18,6 +19,7 @@ stack *push(int x,stack *top){
   p = memory();
   p->data = x;
   p->next = top;
+  printf("push OK\n");
   return p;
 }
 
@@ -32,9 +34,26 @@ stack *pop(stack *top){
     top = top->next;
     printf("pop OK %d\n",x);
   }
+  
+  /*if(top != NULL){
+    x  = top->data;
+    top = top->next;
+    printf("%d\n",x);
+  }
+  else
+    printf("pop NG\n");
+  */
   return top;
 }
 
 int main(void){
+  //int stackdata[5];
+  //int stackpointer = 0;
+  stack *top = NULL;
+
+  top = push(2,top);
+  top = push(5,top);
+  pop(top);
+
   return 0;
 }

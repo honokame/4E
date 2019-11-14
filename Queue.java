ea22639963f7f9d84a6d66c1421efeb43e6031cd
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-11-14 16:25:53 +0900

    Queue生成の関数

--- Queue.java
+++ Queue.java
@@ -1,26 +1,41 @@
 import java.util.*;
 
 public class Queue<T>{
-}
+  private List<T> Queue = new ArrayList<T>();
+  private int QueueSize;
+  private int head;
+  private int tail;
 
-// 指定なし
-public Queue(){
-}
 
-// サイズ指定あり
-public Queue(int size){
-}
+  // 指定なし
+  public Queue(){
+    this.QueueSize = 10;
+    this.head = 0;
+    this.tail = 0;
+  }
 
-private boolean Full(){
-}
+  // サイズ指定あり
+  public Queue(int size){
+    this.QueueSize = size;
+    this.head = 0;
+    this.tail = 0;
+  }
 
-private boolean Empty(){
-}
-// enqueue enqueueできたか返す
-public boolean enqueue(T data){
-  
-}
+  private boolean Full(){
+  }
 
-// dequeue 
-public T dequeue(){
+  private boolean Empty(){
+  }
+  // enqueue enqueueできたか返す
+  public boolean enqueue(T data){
+
+  }
+
+  // dequeue 
+  public T dequeue(){
+  }
+
+  public static void main(String args[]){
+    Queue<String> testQueue = new Queue<String>(2);
+  }
 }

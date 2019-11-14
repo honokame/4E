3b6c4a37cf8aacb3b4da78d190c32036b728afe1
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-11-14 23:11:35 +0900

    コメント

--- Queue.java
+++ Queue.java
@@ -1,21 +1,22 @@
 import java.util.*;
 
 public class Queue<T>{
-  private List<T> Queue = new ArrayList<T>();
-  private int QueueSize;
-  private int head = 0;
-  private int tail= 0;
-  private int count = 0;
+  private List<T> Queue = new ArrayList<T>(); 
+  private int QueueSize; 
+  private int head = 0; 
+  private int tail= 0;　
+    private int count = 0; // Queueに格納されているデータの数
 
-  // 指定なし
   public Queue(){
     this.QueueSize = 10;
   }
 
-  // サイズ指定あり
   public Queue(int size){
-    this.QueueSize = size + 1;
+    this.QueueSize = size + 1; 
   }
+
+  // リングバッファの関係性とデータの数で判断
+  // FullとEmptyが被らない処理
 
   private boolean Full(){
     return this.head == ((this.tail + 1) % this.QueueSize) || this.count == this.QueueSize;
@@ -25,23 +26,22 @@ public class Queue<T>{
     return this.head == this.tail || this.count == 0;
   }
 
-  // enqueue enqueueできたか返す
+  // 操作するごとにcountの数も変わる
+
   public boolean enqueue(T data){
     if(Full()){
       return false;
     }
     else{
-      this.tail++;
       this.Queue.add(data);
-      this.tail = this.tail % this.QueueSize;
+      this.tail = (this.tail + 1) % this.QueueSize; 
       this.count++;
       return true;
     }
   }
 
-  // dequeue 
   public T dequeue(){
-    T send;
+    T send; // 先頭データ記憶用
     if(Empty()){
       return null;
     }
@@ -52,17 +52,18 @@ public class Queue<T>{
       return send;
     }
   }
-
-  public static void main(String args[]){
-    Queue<String> testQueue = new Queue<String>(2);
-    System.out.println(testQueue.enqueue("erumo"));
-    System.out.println(testQueue.enqueue("kuso"));
-    System.out.println(testQueue.enqueue("mikan"));
-    System.out.println(testQueue.dequeue());
-    System.out.println(testQueue.enqueue("ringo"));
-    System.out.println(testQueue.dequeue());
-    System.out.println(testQueue.dequeue());
-    System.out.println(testQueue.dequeue());
-    System.out.println(testQueue.enqueue("ringo"));
-  }
+  　
+    // テスト
+    public static void main(String args[]){
+      Queue<String> testQueue = new Queue<String>(2);
+      System.out.println(testQueue.enqueue("erumo"));
+      System.out.println(testQueue.enqueue("kuso"));
+      System.out.println(testQueue.enqueue("mikan"));
+      System.out.println(testQueue.dequeue());
+      System.out.println(testQueue.enqueue("ringo"));
+      System.out.println(testQueue.dequeue());
+      System.out.println(testQueue.dequeue());
+      System.out.println(testQueue.dequeue());
+      System.out.println(testQueue.enqueue("ringo"));
+    }
 }

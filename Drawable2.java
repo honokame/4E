0a9a4eed2e94f004dc77a289d8ed7a2184c4f4a5
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-11-27 17:25:13 +0900

    仮インターフェース

---
+++ Drawable2.java
@@ -0,0 +1,5 @@
+public interface Drawable2{
+  void draw(int x,int y,int radius,int n,int color);
+  void calcArea(int radius,int n,int x,int y);
+  void display(double s,int radius,int x,int y);
+}

8b20f8477bd85b2526edc575b6664c769e1a2044
okuguchihonoka <honoka.happy.0613@icloud.com>
2019-11-28 04:52:00 +0900

    提出用完成

--- ScoreManagement.java
+++ ScoreManagement.java
@@ -5,18 +5,18 @@ public class ScoreManagement{
   private ArrayList<Integer> scoredata = new ArrayList<Integer>();
   private int max = 0,min = 100,size;
   private double average,median;
+  
   public void save(ArrayList<Integer> data){
     scoredata = new ArrayList<Integer>(data);
-    size = scoredata.size();
+    size = scoredata.size(); //省略のため 
 
     for(int i = 0;i < size;i++){
       max = Math.max(max,scoredata.get(i));
       min = Math.min(min,scoredata.get(i));
       average += scoredata.get(i);
     }
-    
     average = average / size;
-
+    
     if((size % 2) == 0){
       median = (scoredata.get(size / 2) + scoredata.get(size / 2 - 1)) / 2;
     }
